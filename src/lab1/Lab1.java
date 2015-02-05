ab946262bb26235af665afd982c9025d7b79f253
iThure <christiata29@uww.edu>
2015-02-05 16:25:26 -0600

    Actual Program

--- src/lab1/Lab1.java
+++ src/lab1/Lab1.java
@@ -1,5 +1,23 @@
 package lab1;
 
-public class Lab1 {
+import java.util.Scanner;
 
+public class Lab1 {
+	
+	public static void main(String[] args) {
+		String name = "qwerty";
+		
+		System.out.println("Please enter the passcode");
+		Scanner s = new Scanner (System.in);
+		String user_input = s.next();
+		if(user_input == name){
+			System.out.println("Yay you guessed it");
+		}
+		else{
+			System.out.println("Sorry thats not correct");
+		}
+		
+		
+	}
+	
 }
